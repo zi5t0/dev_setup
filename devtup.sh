5249bcc14afa7478441354ad685020fee5f0a51e
zi5t0 <zbotpath@gmail.com>
2022-02-09 21:28:32 +0100

    added docker-compose

--- devtup.sh
+++ devtup.sh
@@ -38,7 +38,7 @@ sudo apt install -y evince libreoffice thunderbird filezilla
 # Docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-sudo apt update && sudo apt install -y docker-ce
+sudo apt update && sudo apt install -y docker-ce docker-compose
 sudo usermod -aG docker $USR
 
 # Qemu, virt-manager
