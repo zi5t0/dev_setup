b15bd917126236e34090d9ccc4b3397187dbab1c
zi5t0 <zbotpath@gmail.com>
2023-08-03 00:37:08 +0200

    Update dev_installer.sh

--- dev_installer.sh
+++ dev_installer.sh
@@ -2,18 +2,25 @@
 
 USERNAME=$(whoami)
 # Setup sudo user
-sudo apt install -y sudo
-sudo /usr/sbin/usermod -aG sudo $USERNAME
+su -c "apt install -y sudo"
+su -c "/usr/sbin/usermod -aG sudo $USERNAME"
 newgrp zhork
-exit
+
 # Install basic programs
 sudo apt install -y build-essential linux-headers-$(uname -r) vim xorg xserver-xorg i3 git libx11-dev libxft-dev libxinerama-dev lxterminal
+
 # Set vim config
 echo ":syntax on">> /home/$USERNAME/.vimrc
 echo ":set number">> /home/$USERNAME/.vimrc
 sudo cp /home/$USERNAME/.vimrc /root/
-# Set screen res
-xrandr --output Virtual1 --mode 3840x2160 --rate 59.97 --scale 0.5x0.5
 
-# Start graphical interface
-startx
+# Set keyboard layout
+setxkbmap es
+
+# Set screen res
+echo "xrandr --output Virtual1 --mode 3840x2160 --rate 59.97 --scale 0.5x0.5" >> ~/.profile
+
+# Install display manager
+sudo apt install -y slim
+sudo systemctl enable slim
+sudo reboot
