7eb6bce5049e085f7552e13c1b774e287e34ae7a
zi5t0 <zbotpath@gmail.com>
2023-08-11 15:22:25 +0200

    Create script_preparatorio.sh

---
+++ script_preparatorio.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# - Instalar git, alacritty
+sudo apt update -y && sudo apt install -y git && sudo apt
+
+# TODO: REPRENSARLO TODO
+# - clonar el proyecto y mover los ficheros
+# - mover ficheros dentro de carpeta git a directorio usuario actual
+# - borrar directorio git clonado
