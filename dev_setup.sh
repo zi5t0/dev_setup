0f8dc1669292bd5d4476920e16428443f62cfae4
zi5t0 <zbotpath@gmail.com>
2023-08-16 23:28:50 +0200

    Update dev_setup.sh

--- dev_setup.sh
+++ dev_setup.sh
@@ -115,6 +115,5 @@ sudo dpkg --add-architecture i386 && sudo apt update -y
 sudo mkdir -pm755 /etc/apt/keyrings
 sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
 sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
-sudo apt update -y
-sudo apt install -y --install-recommends winehq-stable
+sudo apt update -y && sudo apt install -y --install-recommends winehq-stable
 
