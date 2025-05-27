74ad54696a8f75077d97f39a310ce59818573e78
zi5t0 <zbotpath@gmail.com>
2025-05-27 14:22:45 +0200

    Update dev_setup.sh

--- dev_setup.sh
+++ dev_setup.sh
@@ -48,9 +48,12 @@ sudo apt update && sudo apt install -y docker-ce docker-compose
 sudo usermod -aG docker $USR
 
 # Visual Studio Code
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-sudo add-apt-repository -y "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-sudo apt update && sudo apt install -y code
+echo "code code/add-microsoft-repo boolean true" | sudo debconf-set-selections
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+rm -f packages.microsoft.gpg
+sudo apt update && sudo apt install code
 
 # Postman
 wget https://dl.pstmn.io/download/latest/linux64
